fcaaca2109f21f3c87adf41e9948939f989eaa46
Deshraj <deshrajdry@gmail.com>
2016-10-07 08:50:59 -0400

    Urlpattern modified

--- svqa/urls.py
+++ svqa/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('chat.urls')),
-]
+    url(r'^vicki/', include('vicki.urls')),
+    # url(r'^', include('chat.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
